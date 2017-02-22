7aa4e199da6532412bebec75f302201115544b70
james-ciclitira <jciclitira@gmail.com>
2017-02-22 20:05:40 +0000

    suggestv

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,12 +2,13 @@ var bio = {
     "name": "James Ciclitra",
     "role": "Product and interface designer",
     "welcomeMessage": "I am a London based UI designer. I have spend the last year and a half as the design lead for a mobile startup working on both iOS and android platform. In this time I have had the chance to tackle a array of challenges, in a number fields such as UI, UX, motion and visual design as well as working within the marketing department in order to produce promotional product and material. My passion is developing clean, effective and useable interfaces. I am happy and able to work on either a Web, iOS or Android platform but my preference is the former. I am a strong believer of material design principles and lover all things Google. <br><br>Within my current position at CrowdScores, we produce a social football scores network for both Android and iOS platforms. We have a strong mobile development team with two developers per platform. I am not a huge football fan but that has not mattered as my passion is to create a great product irrespective of the field or market. I have been working as the principal designer with feedback and input from the rest of the company. Being the sole design lead has been a challenge at times, but it has been a great learning experience. I have had the opportunity to work closely with an amazing development team and marketing department.<br><br>When I joined CrowdScores we had 47,000 monthly users. We are now approaching half a million, and still growing! It has been a brilliant project to work on. For my career development I have decided that I need to move to a larger team, while still working as an in-house designer for one brand. Although I have learnt a lot about football in the time that I have spent in my current position, I am still not the biggest football fan and am looking for something new. I am naturally inquisitive and always interested in learning new things. At the moment, I am working on a front-end web development Nanodegree offered by Udacity. I am halfway though the course and am keen to follow this with the Android basic Nanodegree.",
+
+    //need to remove array - not sure what to do..
     "contacts": [{
         "email": "Email: jciclitira@gmail.com",
-        "twitter": "Mobile:     07847395121",
-        "location": "Location:     London",
+        "location": "Location: London",
         "mobile": "07847395121",
-        "github": "james-ciclitira"
+        "github": "GitHub: james-ciclitira"
     }],
     "biopic": "images/bio.png",
     "skills": ["Sketching", "UI/UX Design", "Interaction Design", "Prototyping", "Wireframes", "Grid & Layout", "Color Theory", "Typography", "Design Research", "Visual Design"],
@@ -23,7 +24,7 @@ var education = {
         "dates": "2011 - 2015",
         "url": "http://www.eca.ed.ac.uk/school-of-design/undergraduate/product-design-ba-hons"
     }, ],
-    "onlinecourses": [{
+    "onlineCourses": [{
         "title": "Front-End Web Developer Nanodegree",
         "school": "Udacity",
         "dates": "2015 - 2016",
@@ -33,29 +34,39 @@ var education = {
 
 var work = {
     "jobs": [{
-        "employer": "CrowdScores",
-        "title": "Mobile designer",
-        "location": "London - United Kingdom",
-        "dates": "September 2015 - present",
-        "description": "CrowdScores is a real-time sports data application. I had the pleasure of being the design lead and taking responsibility for a variety of roles including, designing the mobile application, conducting research, conceptualising ideas, building mockup prototypes, and finally creating promotional material for the marketing department. On a whole I have enjoyed the time I have spend here, as it has been a valuable learning experience but I feel it is time for a change.",
-        "url": "http://CrowdScores.com/"
-    }, {
-        "employer": "SamLab",
-        "title": "Designer Intern",
-        "location": "London - United Kingdom",
-        "dates": "August 2015 - August 2015",
-        "description": "With SAM smart construction kit, you create awesome inventions with wireless block. I work here for a few weeks after meeting at New Designers to help develop a education package.",
-        "url": "http://SamLab.com/"
-    }]
+            "employer": "Suggestv",
+            "title": "Web designer",
+            "location": "London - United Kingdom",
+            "dates": "January 2017 - present",
+            "description": "Suggestv is the inventor of semantic video distribution. During my time here I have worked on developing a online platform for buying and selling video content. ",
+            "url": "http://sugges.tv/"
+        },
+
+        {
+            "employer": "CrowdScores",
+            "title": "Mobile designer",
+            "location": "London - United Kingdom",
+            "dates": "September 2015 - January 2017",
+            "description": "CrowdScores is a real-time sports data application. I had the pleasure of being the design lead and taking responsibility for a variety of roles including, designing the mobile application, conducting research, conceptualising ideas, building mockup prototypes, and finally creating promotional material for the marketing department. On a whole I have enjoyed the time I have spend here, as it has been a valuable learning experience but I feel it is time for a change.",
+            "url": "http://CrowdScores.com/"
+        }, {
+            "employer": "SamLab",
+            "title": "Designer Intern",
+            "location": "London - United Kingdom",
+            "dates": "August 2015 - August 2015",
+            "description": "With SAM smart construction kit, you create awesome inventions with wireless block. I work here for a few weeks after meeting at New Designers to help develop a education package.",
+            "url": "http://SamLab.com/"
+        }
+    ]
 };
 
 var project = {
     "project": [{
-        "title": "Udacity portfolio project",
+        "title": "Suggestv - web site",
         "dates": "2016",
-        "description": "The portfolio page, and it was the first project undertake for Udacity's Front-End Nanodegree. The project was built on Google App Engine with Bootstrap, with a focus on using Bootstrap’s grid styling system to style a responsive and organized wep page more easily.",
-        "images": ["images/web1.png", "images/web2.png", "images/web3.png"],
-        "url": "https://github.com/james-ciclitira/udacity_protfolio_project"
+        "description": "Front facing web site for suggestv",
+        "images": ["images/suggestv-1.png", "images/suggestv-2.png", "images/suggestv-3.png"],
+        "url": "https://sugges.tv"
     }, {
         "title": "CrowdScores mobile design",
         "dates": "2016",
@@ -129,7 +140,6 @@ bio.display = function() {
 
 };
 
-
 bio.display();
 
 work.display = function() {
@@ -170,11 +180,11 @@ education.display = function() {
 
     $("#education").append(HTMLonlineClasses);
 
-    education.onlinecourses.forEach(function(onlinecourses) {
-        var formattedTitle = HTMLonlineTitle.replace("%data%", onlinecourses.title).replace("#", onlinecourses.url);
-        var formattedSchool = HTMLonlineSchool.replace("%data%", onlinecourses.school);
+    education.onlineCourses.forEach(function(onlineCourses) {
+        var formattedTitle = HTMLonlineTitle.replace("%data%", onlineCourses.title).replace("#", onlineCourses.url);
+        var formattedSchool = HTMLonlineSchool.replace("%data%", onlineCourses.school);
         var formattedTitleSchool = formattedTitle + formattedSchool;
-        var formattedDate = HTMLonlineDates.replace("%data%", onlinecourses.dates);
+        var formattedDate = HTMLonlineDates.replace("%data%", onlineCourses.dates);
 
         $("#education").append(HTMLschoolStart);
         $(".education-entry:last").append(formattedTitleSchool);
