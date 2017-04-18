2c8f86fbdbfc40fce477d863213fd3ba9f0de2a2
james-ciclitira <jciclitira@gmail.com>
2017-04-18 12:11:34 +0100

    removing bootstrap

--- js/helper.js
+++ js/helper.js
@@ -1,158 +1,30 @@
-var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<h3 >%data%</h3>';
+var HTMLheaderName = '<h1 class="mb2">%data%</h1>';
+var HTMLheaderRole = '<h3 class="mv0 f3 fw3">%data%</h3>';
 
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="contacts">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="contacts">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="contacts">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="contacts">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="contacts">%data%</span></li>';
-var HTMLlocation = '<li data-type="number" class="flex-item"><span class="contacts">%data%</span></li>';
+var HTMLlocation = '<li class="list f6 ma2" data-type="number"><span class="contacts">%data%</span></li>';
+var HTMLemail = '<li class="list f6 ma2"><span>%data%</span></li>';
+var HTMLgithub = '<li class="list f6 ma2"><span>%data%</span></li>';
+var HTMLmobile = '<li class="list f6 ma2"><span>%data%</span></li>';
 
-var HTMLbioPic = "<img src='%data%' class='biopic'>";
 var HTMLabout = '<hr><h3>About</h3><p id="about">%data%</<p><hr>';
-
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
-var HTMLskills = '<li class="skills"><span>%data%</span></li>';
-var development ='<div class="media "><a class="media-left" "><img class="media-object" src=%data%"></a><div class="media-body "><h4 class="media-heading">"%title%"</h4>"%body%"</div></div>';
+var HTMLskills = '<li class="list pv2">%data%</li>';
+var HTMLtools = '<a class="w-50 mt3 dib"><img style="height: 40px;" src=#><h3 class="dib mt2 ml3 v-top">%data%</h3></a>';
+var HTMLdevelopment = '<a class="w-50 mt3 dib"><img style="height: 40px;" src=#><h3 class="dib mt2 ml3 v-top">%data%</h3></a>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
-var HTMLworkTitle = ' - %data%</a>';
-var HTMLworkDates = '<div class="date-text">%data%</div>';
-var HTMLworkLocation = '<div class="location-text">%data%</div>';
-var HTMLworkDescription = '<p><br>%data%</p>';
+var HTMLworkEmployer = '<a class="no-underline db f5 fw5 pt2" href="#">%data%';
+var HTMLworkTitle = ' &#8226; %data%</a>';
+var HTMLworkDescription = '<p class="ma0 pt2">%data%</p>';
+var HTMLworkDates = '<p class="f6 mt0 pt1">%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
-var HTMLprojectDates = '<div class="date-text">%data%</div>';
+var HTMLprojectDates = '<div>%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12"><div class="card"><div class="card-image"><img src="%data%">';
+var HTMLprojectImage = '<div class="w-100 w-third-ns dib"><div><div><img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = "<a href='#'>%data%";
-var HTMLschoolDegree = ' - %data%</a>';
-var HTMLschoolDates = '<div class="date-text">%data%</div>';
-var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em><br>Degree: %data%</em>';
-
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = "<a href='#'>%data%";
-var HTMLonlineSchool = ' - %data%</a>';
-var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
-
-var internationalizeButton = '<button>Internationalize</button>';
-var googleMap = '<div id="map"></div>';
-
-var HTMLtools = '<div class="media"><a class="media-left"><img class="media-object" src=#></a><div class="media-body"><h4 class="media-heading-2">%data%</h4></div></div>';
-var HTMLtools2 = '<div class="media"><a class="media-left"><img class="media-object" src=#></a><div class="media-body"><h4 class="media-heading-2">%data%</h4></div></div>';
-var HTMLdevelopment = '<div class="media"><a class="media-left"><img class="media-object" src=#></a><div class="media-body"><h4 class="media-heading-2">%data%</h4></div></div>';
-
-$(document).ready(function() {
-  $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);
-  });
-});
-
-clickLocations = [];
-
-function logClicks(x,y) {
-  clickLocations.push(
-    {
-      x: x,
-      y: y
-    }
-  );
-  console.log('x location: ' + x + '; y location: ' + y);
-}
-
-$(document).click(function(loc) {
-  logClicks(loc.pageX, loc.pageY)
-});
-
-
-
-var map;
-
-function initializeMap() {
-
-  var locations;
-
-  var mapOptions = {
-    disableDefaultUI: true
-  };
-
-  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
-
-  function locationFinder() {
-
-    var locations = [];
-
-    education.schools.forEach(function(school){
-      locations.push(school.location);
-    });
-
-    work.jobs.forEach(function(job){
-      locations.push(job.location);
-    });
-
-    return locations;
-  }
-
-
-  function createMapMarker(placeData) {
-
-    var lat = placeData.geometry.location.lat();
-    var lon = placeData.geometry.location.lng();
-    var name = placeData.formatted_address;
-    var bounds = window.mapBounds;
-
-    var marker = new google.maps.Marker({
-      map: map,
-      position: placeData.geometry.location,
-      title: name
-    });
-
-    var infoWindow = new google.maps.InfoWindow({
-      content: name
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-    });
-
-    bounds.extend(new google.maps.LatLng(lat, lon));
-    map.fitBounds(bounds);
-    map.setCenter(bounds.getCenter());
-  }
-
-    function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      createMapMarker(results[0]);
-    }
-  }
-
-  function pinPoster(locations) {
-    var service = new google.maps.places.PlacesService(map);
- locations.forEach(function(place){
-      var request = {
-        query: place
-      };
-
-      service.textSearch(request, callback);
-    });
-  }
-
-  window.mapBounds = new google.maps.LatLngBounds();
-
-  locations = locationFinder();
-
-  pinPoster(locations);
-
-}
-
-window.addEventListener('load', initializeMap);
-
-
-
-
+var HTMLschoolName = "<a class='no-underline db' href='#'>%data%</a>";
+var HTMLschoolDegree = '<p class="dib">%data%</p>';
+var HTMLschoolMajor = '<p class="dib ml1"> &#8226; %data%</p>';
+var HTMLschoolDates = '<p class="mt0">%data%</p>';
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,60 +1,61 @@
 var bio = {
     "name": "James Ciclitra",
-    "role": "Product and interface designer",
+    "role": "Digital Product designer",
     "welcomeMessage": "I am a London based UI designer. I have spend the last year and a half as the design lead for a mobile startup working on both iOS and android platform. In this time I have had the chance to tackle a array of challenges, in a number fields such as UI, UX, motion and visual design as well as working within the marketing department in order to produce promotional product and material. My passion is developing clean, effective and useable interfaces. I am happy and able to work on either a Web, iOS or Android platform but my preference is the former. I am a strong believer of material design principles and lover all things Google. <br><br>Within my current position at CrowdScores, we produce a social football scores network for both Android and iOS platforms. We have a strong mobile development team with two developers per platform. I am not a huge football fan but that has not mattered as my passion is to create a great product irrespective of the field or market. I have been working as the principal designer with feedback and input from the rest of the company. Being the sole design lead has been a challenge at times, but it has been a great learning experience. I have had the opportunity to work closely with an amazing development team and marketing department.<br><br>When I joined CrowdScores we had 47,000 monthly users. We are now approaching half a million, and still growing! It has been a brilliant project to work on. For my career development I have decided that I need to move to a larger team, while still working as an in-house designer for one brand. Although I have learnt a lot about football in the time that I have spent in my current position, I am still not the biggest football fan and am looking for something new. I am naturally inquisitive and always interested in learning new things. At the moment, I am working on a front-end web development Nanodegree offered by Udacity. I am halfway though the course and am keen to follow this with the Android basic Nanodegree.",
-
-    //need to remove array - not sure what to do..
     "contacts": [{
         "email": "Email: jciclitira@gmail.com",
         "location": "Location: London",
-        "mobile": "07847395121",
+        "mobile": "Mobile: 07847395121",
         "github": "GitHub: james-ciclitira"
     }],
-    "biopic": "images/bio.png",
     "skills": ["Sketching", "UI/UX Design", "Interaction Design", "Prototyping", "Wireframes", "Grid & Layout", "Color Theory", "Typography", "Design Research", "Visual Design"],
-    "tools": ["Abobe Photoshop", "Abobe Illustrator", "Abobe Indesign", "Sketch", "Invision", "SolidWorks", "Principle"]
 };
 
 var education = {
     "schools": [{
         "name": "Edinburgh College Of Art",
-        "location": "Edinburgh",
         "degree": "Product Design",
-        "majors": "2:1 BA (Hons)",
+        "majors": "2:1 ba (Hons)",
         "dates": "2011 - 2015",
         "url": "http://www.eca.ed.ac.uk/school-of-design/undergraduate/product-design-ba-hons"
-    }, ],
-    "onlineCourses": [{
-        "title": "Front-End Web Developer Nanodegree",
-        "school": "Udacity",
-        "dates": "2015 - 2016",
-        "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001?v=fe1"
-    }]
+    }, ]
 };
 
 var work = {
     "jobs": [{
             "employer": "Suggestv",
-            "title": "Web designer",
+            "title": "Product & Web development",
             "location": "London - United Kingdom",
-            "dates": "January 2017 - present",
+            "dates": "January 2017 - April 2017",
             "description": "Suggestv is the inventor of semantic video distribution. During my time here I have worked on developing a online platform for buying and selling video content. ",
             "url": "http://sugges.tv/"
         },
 
         {
             "employer": "CrowdScores",
-            "title": "Mobile designer",
-            "location": "London - United Kingdom",
+            "title": "Product designer",
             "dates": "September 2015 - January 2017",
             "description": "CrowdScores is a real-time sports data application. I had the pleasure of being the design lead and taking responsibility for a variety of roles including, designing the mobile application, conducting research, conceptualising ideas, building mockup prototypes, and finally creating promotional material for the marketing department. On a whole I have enjoyed the time I have spend here, as it has been a valuable learning experience but I feel it is time for a change.",
             "url": "http://CrowdScores.com/"
         }, {
+            "employer": "Prediction labs",
+            "title": "Freelance designer",
+            "dates": "",
+            "description": "Numilli app - Bitcoin safe and easy. Numilli  aims to makes it easy to securely buy and sell bitcoin currency, on the go using a mobile app & ATM machine.",
+        },
+        {
+            "employer": "Foreminds",
+            "title": "Freelance designer",
+            "dates": "",
+            "description": "BaseSight - workforce management platform, which aims to help improve efficiency & productivity",
+        },
+
+
+        {
             "employer": "SamLab",
             "title": "Designer Intern",
-            "location": "London - United Kingdom",
             "dates": "August 2015 - August 2015",
-            "description": "With SAM smart construction kit, you create awesome inventions with wireless block. I work here for a few weeks after meeting at New Designers to help develop a education package.",
+            "description": "",
             "url": "http://SamLab.com/"
         }
     ]
@@ -82,21 +83,23 @@ var tools = {
         "title": "Sketch",
         "image": ["images/sketch.svg"]
     }, {
-        "title": "Photoshop",
-        "image": ["images/Photoshop.svg"]
-    }]
-};
-
-var tools2 = {
-    "tools2": [{
         "title": "Illustrator",
         "image": ["images/Illustrator.svg"]
     }, {
+        "title": "Photoshop",
+        "image": ["images/Photoshop.svg"]
+    }, {
         "title": "Invision",
         "image": ["images/invision.svg"]
+    }, {
+        "title": "Pixate",
+        "image": ["images/pixate.png"]
+    }, {
+        "title": "Craft",
+        "image": ["images/craft.svg"]
     }]
-};
 
+};
 
 var development = {
     "development": [{
@@ -105,6 +108,9 @@ var development = {
     }, {
         "title": "CSS",
         "image": ["images/css.svg"]
+    }, {
+        "title": "React",
+        "image": ["images/react.svg"]
     }, {
         "title": "Javascript",
         "image": ["images/js.svg"]
@@ -120,16 +126,11 @@ bio.display = function() {
     bio.contacts.forEach(function(contacts) {
         var Location = HTMLlocation.replace("%data%", contacts.location);
         var Email = HTMLemail.replace("%data%", contacts.email);
-        var Twitter = HTMLtwitter.replace("%data%", contacts.twitter);
         var Github = HTMLgithub.replace("%data%", contacts.github);
-        $("#topContacts, #footer").append(Location).append(Email).append(Github);
+        var Mobile = HTMLmobile.replace("%data%", contacts.mobile);
+        $("#topContacts, #footer").append(Location).append(Email).append(Github).append(Mobile);
 
     });
-
-    //   for (var skills in bio.skills) {
-    //       var Skills = HTMLskills.replace("%data%", bio.skills[skills]);
-    //       $("#skills").append(Skills);
-    //   }
 
     bio.skills.forEach(function(skills) {
         $("#skills").append(HTMLskills.replace("%data%", skills));
@@ -150,11 +151,10 @@ work.display = function() {
         work.jobs.forEach(function(jobs) {
             var formattedEmployer = HTMLworkEmployer.replace("%data%", jobs.employer).replace("#", jobs.url);
             var formattedTitle = HTMLworkTitle.replace("%data%", jobs.title);
-            var formattedDates = HTMLworkDates.replace("%data%", jobs.dates);
             var formattedDescription = HTMLworkDescription.replace("%data%", jobs.description);
-            var location = HTMLworkLocation.replace("%data%", jobs.location);
+            var formattedDates = HTMLworkDates.replace("%data%", jobs.dates);
 
-            $(".work-entry:last").append(formattedEmployer + formattedTitle, formattedDates, formattedDescription, location);
+            $(".work-entry:last").append(formattedEmployer + formattedTitle, formattedDescription, formattedDates);
         });
     }
 };
@@ -170,27 +170,12 @@ education.display = function() {
         var formattedNameDegree = formattedSchoolName + formattedDegree;
         var formattedSchoolDate = HTMLschoolDates.replace("%data%", school.dates);
         var formattedMajor = HTMLschoolMajor.replace("%data%", school.majors);
-        var formattedLocation = HTMLschoolLocation.replace("%data%", school.location);
 
         $(".education-entry:last").append(formattedNameDegree);
         $(".education-entry:last").append(formattedMajor);
-        $(".education-entry:last").append(formattedLocation);
         $(".education-entry:last").append(formattedSchoolDate);
     });
 
-    $("#education").append(HTMLonlineClasses);
-
-    education.onlineCourses.forEach(function(onlineCourses) {
-        var formattedTitle = HTMLonlineTitle.replace("%data%", onlineCourses.title).replace("#", onlineCourses.url);
-        var formattedSchool = HTMLonlineSchool.replace("%data%", onlineCourses.school);
-        var formattedTitleSchool = formattedTitle + formattedSchool;
-        var formattedDate = HTMLonlineDates.replace("%data%", onlineCourses.dates);
-
-        $("#education").append(HTMLschoolStart);
-        $(".education-entry:last").append(formattedTitleSchool);
-        $(".education-entry:last").append(formattedDate);
-
-    });
 };
 
 education.display();
@@ -209,12 +194,6 @@ projects.display = function() {
         item.images.forEach(function(img) {
             $(".project-entry:last").append(HTMLprojectImage.replace("%data%", img));
         });
-
-        // for (var images in projects.images) {
-        //     var images = HTMLprojectImage.replace("%data%", projects.images[images]);
-        //     $(".project-entry:last").append(images);
-        // }
-
 
     });
 };
@@ -241,16 +220,3 @@ tools.display = function() {
 };
 
 tools.display();
-
-
-tools2.display = function() {
-
-    tools2.tools2.forEach(function(tools2) {
-        var formattedTools2 = HTMLtools2.replace("%data%", tools2.title).replace("#", tools2.image);
-        $("#design-tools2").append(formattedTools2);
-    });
-};
-
-tools2.display();
-
-$("#mapDiv").append(googleMap);
