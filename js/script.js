2170f89a03aa68a9456479267bfc083bd52a1648
james-ciclitira <jciclitira@gmail.com>
2017-09-17 10:50:16 +0100

    cv changes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-var HTMLheaderName = '<h1>%data%</h1>';
-var HTMLabout = '<h3 class="f4-ns f5 fw3" style="letter-spacing:.05em">%data%</<h3>';
+var HTMLheaderName = '<h2>%data%</h2>';
+var HTMLabout = '<p class="f4-ns f5 fw3">%data%</<p>';
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkTitle = '<h3 class="black-80 mb1 mt4 fw6">%data%</h3>';
 var HTMLworkEmployer = '<a class="no-underline red db f5 fw5 pt1" href="#">%data%';
-var HTMLworkDates = '<span class="black-50"> &#8226; %data%</span> </a>';
+var HTMLworkDates = '<span class="black-50"> - %data%</span> </a>';
 var HTMLworkDescription = '<p class="ma0 pt2">%data%</p>';
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = "<a class='no-underline red f5 fw5 db mt0 pt0' href='#'>%data%";
-var HTMLschoolDates = '<span class="black-50"> &#8226; %data%</span> </a>';
+var HTMLschoolDates = '<span class="black-50"> - %data%</span></a>';
 var HTMLschoolDegree = '<p class="dib">%data%</p>';
 var HTMLschoolMajor = '<p class="dib ml1"> - %data%</p>';
 var HTMLskills = '<li class="list black-50 pv2">%data%</li>';
